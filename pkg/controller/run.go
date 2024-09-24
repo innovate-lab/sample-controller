3427eb16fb89b7b4d16333bc54cf2a710536dfec
lauxinchi <lauxinchi@gmail.com>
2024-09-24 15:51:50 +0800

    resove vendor

--- pkg/controller/run.go
+++ pkg/controller/run.go
@@ -2,9 +2,17 @@ package controller
 
 import (
 	"flag"
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 	"sample-controller/pkg/signals"
 	"time"
+	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	clientset "sample-controller/pkg/generated/clientset/versioned"
+	informers "sample-controller/pkg/generated/informers/externalversions"
 )
 
 var (
@@ -54,4 +62,6 @@ func Run() error {
 		logger.Error(err, "Error running controller")
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
+
+	return nil
 }
